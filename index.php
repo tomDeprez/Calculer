ef96080bde702572b74b57757998d4a21f34c199
Tom DEPREZ <tom_deprez@hotmail.fr>
2025-04-28 14:44:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-3" style="background-color: red; height: 150px;"></div>
+            <div class="col-3" style="background-color: black; height: 150px;">
+                <div class="row">
+                    <div class="12" style="background-color: blue; height: 25px;"></div>
+                </div>
+            </div>
+            <div class="col" style="background-color: blue; height: 150px;"></div>
+            <div class="col-md-3" style="background-color: red; height: 150px;"><h1>salut</h1></div>
+            <div class="col-12" style="height: 50px;"></div>
+            <div class="col-12" style="background-color: black; height: 150px;"></div>
+            <div class="col" style="background-color: red; height: 75px;"></div>
+            <div class="col" style="background-color: blue; height: 75px;"></div>
+            <div class="col-12" style="height: 100px;"></div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+
+            <div class="row bg-gray-total">
+                <div class="col-12" style="height: 25px;"></div>
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <div class="row bg-gold">
+                        <div class="col"><h4>12*12</h4></div>
+                        <div class="col d-flex justify-content-end">
+                            <h1>10</h1>    
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-12" style="height: 25px;"></div>
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-2"><button>7</button></div>
+                        <div class="col-2"><button>8</button></div>
+                        <div class="col-2"><button>9</button></div>
+                        <div class="col-2"></div>
+                        <div class="col-2"><button>CE</button></div>
+                        <div class="col-2"><button>C</button></div>
+
+                        <div class="col-12" style="height: 15px;"></div>
+
+                        <div class="col-2"><button>4</button></div>
+                        <div class="col-2"><button>5</button></div>
+                        <div class="col-2"><button>6</button></div>
+                        <div class="col-2"></div>
+                        <div class="col-2"><button>/</button></div>
+                        <div class="col-2"><button>*</button></div>
+
+
+                        <div class="col-12" style="height: 15px;"></div>
+
+                        <div class="col-2"><button>3</button></div>
+                        <div class="col-2"><button>2</button></div>
+                        <div class="col-2"><button>1</button></div>
+                        <div class="col-2"></div>
+                        <div class="col-2"><button>-</button></div>
+                        <div class="col-2"><button>Calculer</button></div>
+                        <div class="col-12" style="height: 35px;"></div>
+                    </div>
+                </div>
+            </div>
+
+            </div>
+            <div class="col-3">
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
