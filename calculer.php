3844a6a83abc6c19b90a07092601ff55997d3a80
Tom DEPREZ <tom_deprez@hotmail.fr>
2025-04-29 10:48:24 +0200

    modifications

--- calculer.php
+++ calculer.php
@@ -1,7 +1,7 @@
 <?php
-$calcul = $_GET['calcul'];
+$calcul = $_GET['calcul']; # string
 
-if (str_contains($calcul, '*')) {
+if (str_contains($calcul, '*')) { # true si * dans le string
     echo "multiplication";
     $tblCalcul = explode("*", $calcul);
     echo $tblCalcul[0] * $tblCalcul[1];
